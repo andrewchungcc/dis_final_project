e747fc590e3caf315f8b3efc80e055e02c3651fc
“Xiugapurin” <jason32567810@gmail.com>
2024-12-17 18:10:10 +0800

    update db connection config

--- config.py
+++ config.py
@@ -1,8 +1,19 @@
 import os
 from dotenv import load_dotenv
 
+
 load_dotenv()
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
+    DB_USER = os.getenv("DB_USER")
+    DB_PASS = os.getenv("DB_PASS")
+    DB_NAME = os.getenv("DB_NAME")
+    INSTANCE_UNIX_SOCKET = os.getenv("INSTANCE_UNIX_SOCKET")
+
+    SQLALCHEMY_DATABASE_URI = (
+        f"postgresql+pg8000://{DB_USER}:{DB_PASS}@/{DB_NAME}"
+        f"?unix_sock={INSTANCE_UNIX_SOCKET}/.s.PGSQL.5432"
+    )
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
